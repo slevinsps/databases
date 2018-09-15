cacc58d0b10f0e329ee8199436898cef9eac39b8
Ivan <spasenovivan@mail.ru>
2018-09-15 10:02:18 +0300

    add identity

--- laba1/create.sql
+++ laba1/create.sql
@@ -25,7 +25,7 @@ GO
 -- Create the table in the specified schema
 CREATE TABLE dbo.Films
 (
-    ID int NOT NULL PRIMARY KEY, 
+    ID int IDENTITY(1,1) PRIMARY KEY,
     Title VARCHAR(30) NOT NULL,
     Genre VARCHAR(30),
     Country VARCHAR(30), 
@@ -42,7 +42,7 @@ GO
 -- Create the table in the specified schema
 CREATE TABLE dbo.Actors
 (
-    ID int NOT NULL PRIMARY KEY, 
+    ID int IDENTITY(1,1) PRIMARY KEY, 
     Name VARCHAR(30) NOT NULL,
     Birthdate datetime,
     Rating Int, 
@@ -56,7 +56,7 @@ GO
 -- Create the table in the specified schema
 CREATE TABLE dbo.Studios
 (
-    ID int NOT NULL PRIMARY KEY, 
+    ID int IDENTITY(1,1) PRIMARY KEY, 
     Name VARCHAR(30) NOT NULL,
 	Country VARCHAR(30), 
 	Owner VARCHAR(30), 
@@ -69,12 +69,26 @@ GO
 -- Create the table in the specified schema
 CREATE TABLE dbo.Contracts
 (
-    ID INT NOT NULL PRIMARY KEY, 
+    ID INT IDENTITY(1,1) PRIMARY KEY, 
     Actor INT NOT NULL REFERENCES Actors (ID),
 	Film INT NOT NULL REFERENCES Films (ID),
-	Fee Int,
+	Fee Money,
 	Work_days Int,
 	Studio INT NOT NULL REFERENCES Studios (ID)
 );
 GO
 
+
+ALTER TABLE dbo.Films ADD
+CONSTRAINT Budget_chk CHECK (Budget > 0),
+CONSTRAINT Gross_chk CHECK (Gross >= 0)
+GO
+
+ALTER TABLE dbo.Studios ADD
+CONSTRAINT Empl_chk CHECK (Employees_number > 0)
+GO
+
+ALTER TABLE dbo.Contracts ADD
+CONSTRAINT Fee_chk CHECK (Fee > 0),
+CONSTRAINT Work_chk CHECK (Work_days > 0)
+GO
